8ff0c889b75c317eb5d93ee0dbaf0b5485f871fc
kai <kai@turbot.com>
2025-02-17 11:56:11 +0000

    simplified custom table registration

--- nginx/plugin.go
+++ nginx/plugin.go
@@ -1,7 +1,6 @@
 package nginx
 
 import (
-	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/tailpipe-plugin-nginx/tables/access_log"
 	"github.com/turbot/tailpipe-plugin-sdk/plugin"
 	"github.com/turbot/tailpipe-plugin-sdk/table"
@@ -13,10 +12,7 @@ func init() {
 	// Register the table, with type parameters:
 	// 1. row struct type
 	// 2. table implementation type
-	// And function parameters:
-	// 1. table definition
-	// 2. format
-	table.RegisterCustomTable[*table.DynamicRow, *access_log.AccessLogTable](access_log.AccessLogTableDef, access_log.AccessLogFormat)
+	table.RegisterCustomTable[*table.DynamicRow, *access_log.AccessLogTable]()
 }
 
 type Plugin struct {
@@ -24,12 +20,6 @@ type Plugin struct {
 }
 
 func NewPlugin() (_ plugin.TailpipePlugin, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = helpers.ToError(r)
-		}
-	}()
-
 	p := &Plugin{
 		PluginImpl: plugin.NewPluginImpl(PluginName),
 	}
--- tables/access_log/access_log_table.go
+++ tables/access_log/access_log_table.go
@@ -4,6 +4,7 @@ import (
 	"github.com/turbot/tailpipe-plugin-sdk/artifact_source"
 	"github.com/turbot/tailpipe-plugin-sdk/constants"
 	"github.com/turbot/tailpipe-plugin-sdk/formats"
+	"github.com/turbot/tailpipe-plugin-sdk/parse"
 	"github.com/turbot/tailpipe-plugin-sdk/row_source"
 	"github.com/turbot/tailpipe-plugin-sdk/schema"
 	"github.com/turbot/tailpipe-plugin-sdk/table"
@@ -11,100 +12,6 @@ import (
 )
 
 const AccessLogTableIdentifier = "nginx_access_log"
-
-var AccessLogFormat = &formats.Grok{
-	Layout: `%{IPORHOST:remote_addr} - %{DATA:remote_user} \[%{HTTPDATE:time_local}\] "%{WORD:method} %{DATA:request} HTTP/%{NUMBER:http_version}" %{NUMBER:status} %{NUMBER:body_bytes_sent} "%{DATA:http_referer}" "%{DATA:http_user_agent}"`,
-}
-
-var AccessLogTableDef = &types.CustomTableDef{
-	Name: AccessLogTableIdentifier,
-	Schema: &schema.RowSchema{
-		Columns: []*schema.ColumnSchema{
-			{
-				ColumnName: "tp_timestamp",
-				SourceName: "time_local",
-			},
-			{
-				ColumnName: "tp_date",
-				SourceName: "time_local",
-			},
-			{
-				SourceName: "remote_addr",
-				ColumnName: "tp_source_ip",
-			},
-			{
-				ColumnName: "tp_ips",
-				SourceName: "remote_addr",
-			},
-			{
-				ColumnName: "tp_usernames",
-				SourceName: "remote_user",
-			},
-			{
-				ColumnName: "tp_domains",
-				SourceName: "path",
-			},
-			{
-				ColumnName: "tp_akas",
-				SourceName: "path",
-			},
-			{
-				ColumnName:  "remote_addr",
-				Description: "Original source IP from log",
-			},
-			{
-				ColumnName:  "remote_user",
-				Description: "User authenticated in request",
-			},
-			{
-				ColumnName:  "time_local",
-				Description: "Timestamp in local format",
-			},
-			{
-				ColumnName:  "time_iso_8601",
-				Description: "Timestamp in ISO8601 format",
-			},
-			{
-				ColumnName:  "request",
-				Description: "Full request string",
-			},
-			{
-				ColumnName:  "method",
-				Description: "HTTP method (GET, POST, etc.)",
-			},
-			{
-				ColumnName:  "path",
-				Description: "URL path from request",
-			},
-			{
-				ColumnName:  "http_version",
-				Description: "HTTP version",
-			},
-			{
-				ColumnName:  "status",
-				Description: "HTTP response status code",
-			},
-			{
-				ColumnName:  "body_bytes_sent",
-				Description: "Size of response in bytes",
-			},
-			{
-				ColumnName:  "http_referer",
-				Description: "Referer URL",
-			},
-			{
-				ColumnName:  "http_user_agent",
-				Description: "User agent string",
-			},
-			{
-				ColumnName:  "timestamp",
-				Description: "Parsed timestamp",
-			},
-		},
-		// do not automap - only include specific columns
-		AutoMapSourceFields: false,
-	},
-}
 
 // AccessLogTable - table for nginx access logs
 type AccessLogTable struct {
@@ -115,6 +22,104 @@ func (c *AccessLogTable) Identifier() string {
 	return AccessLogTableIdentifier
 }
 
+func (c *AccessLogTable) GetFormat() parse.Config {
+	return &formats.Grok{
+		Layout: `%{IPORHOST:remote_addr} - %{DATA:remote_user} \[%{HTTPDATE:time_local}\] "%{WORD:method} %{DATA:request} HTTP/%{NUMBER:http_version}" %{NUMBER:status} %{NUMBER:body_bytes_sent} "%{DATA:http_referer}" "%{DATA:http_user_agent}"`,
+	}
+}
+
+func (c *AccessLogTable) GetTableDef() *types.CustomTableDef {
+	return &types.CustomTableDef{
+		Name: AccessLogTableIdentifier,
+		Schema: &schema.RowSchema{
+			Columns: []*schema.ColumnSchema{
+				{
+					ColumnName: "tp_timestamp",
+					SourceName: "time_local",
+				},
+				{
+					ColumnName: "tp_date",
+					SourceName: "time_local",
+				},
+				{
+					SourceName: "remote_addr",
+					ColumnName: "tp_source_ip",
+				},
+				{
+					ColumnName: "tp_ips",
+					SourceName: "remote_addr",
+				},
+				{
+					ColumnName: "tp_usernames",
+					SourceName: "remote_user",
+				},
+				{
+					ColumnName: "tp_domains",
+					SourceName: "path",
+				},
+				{
+					ColumnName: "tp_akas",
+					SourceName: "path",
+				},
+				{
+					ColumnName:  "remote_addr",
+					Description: "Original source IP from log",
+				},
+				{
+					ColumnName:  "remote_user",
+					Description: "User authenticated in request",
+				},
+				{
+					ColumnName:  "time_local",
+					Description: "Timestamp in local format",
+				},
+				{
+					ColumnName:  "time_iso_8601",
+					Description: "Timestamp in ISO8601 format",
+				},
+				{
+					ColumnName:  "request",
+					Description: "Full request string",
+				},
+				{
+					ColumnName:  "method",
+					Description: "HTTP method (GET, POST, etc.)",
+				},
+				{
+					ColumnName:  "path",
+					Description: "URL path from request",
+				},
+				{
+					ColumnName:  "http_version",
+					Description: "HTTP version",
+				},
+				{
+					ColumnName:  "status",
+					Description: "HTTP response status code",
+				},
+				{
+					ColumnName:  "body_bytes_sent",
+					Description: "Size of response in bytes",
+				},
+				{
+					ColumnName:  "http_referer",
+					Description: "Referer URL",
+				},
+				{
+					ColumnName:  "http_user_agent",
+					Description: "User agent string",
+				},
+				{
+					ColumnName:  "timestamp",
+					Description: "Parsed timestamp",
+				},
+			},
+			// do not automap - only include specific columns
+			AutoMapSourceFields: false,
+		},
+	}
+}
+
 func (c *AccessLogTable) GetSourceMetadata() ([]*table.SourceMetadata[*table.DynamicRow], error) {
 	// ask our CustomTableImpl for the mapper
 	mapper, err := c.GetMapper()
@@ -122,6 +127,7 @@ func (c *AccessLogTable) GetSourceMetadata() ([]*table.SourceMetadata[*table.Dyn
 		return nil, err
 	}
 
+	// which source do we support?
 	return []*table.SourceMetadata[*table.DynamicRow]{
 		{
 			// any artifact source
